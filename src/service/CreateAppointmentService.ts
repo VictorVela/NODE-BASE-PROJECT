7704de997b168773b84beb2f21c2cb57dafe14f1
Victor Vela <victorgvela@hotmail.com>
2020-08-02 18:49:16 -0300

    primeira etapa do back concluido

--- src/service/CreateAppointmentService.ts
+++ src/service/CreateAppointmentService.ts
@@ -1,4 +1,7 @@
+import { startOfHour } from 'date-fns';
+
 import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 // parei no 16:40
 
@@ -8,20 +11,24 @@ interface Request {
 }
 
 class CreateAppointmentServise {
-  constructor() {}
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
 
   public execute({ date, provider }: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(
-      parsedDate,
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
     );
 
     if (findAppointmentInSameDate) {
       throw Error('This appointment is already booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = this.appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
